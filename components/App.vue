d474ce35a2677d1015ceed4918b2c789ff160fe6
serhiiko <serhiiko@wix.com>
2020-07-25 22:21:47 +0300

    first commit

---
+++ components/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Vue component v2
+    <p>{{name}}</p>
+    <Nested count="3" />
+  </div>
+</template>
+
+<script>
+  import Nested from './Nested';
+  export default {
+    components: {
+      Nested,
+    },
+    props: {
+      name: {
+        type: String,
+        default: 'serhiiko',
+      },
+    }
+  }
+</script>
---
+++ components/Nested.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>nested {{count}}</p>
+</template>
+
+<script>
+  export default {
+    name: 'Nested',
+    props: {
+      count: {
+        type: Number,
+        default: 0,
+      },
+    }
+  }
+</script>
